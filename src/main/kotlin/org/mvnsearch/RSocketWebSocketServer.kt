3f04534f31ddeae39b6505139a114c4dbdc83dc6
linux_china <libing.chen@gmail.com>
2020-10-21 17:36:33 -0700

    add Ktor module

--- src/main/kotlin/org/mvnsearch/RSocketWebSocketServer.kt
+++ src/main/kotlin/org/mvnsearch/RSocketWebSocketServer.kt
@@ -1,21 +1,37 @@
 package org.mvnsearch
 
-import io.ktor.application.install
-import io.ktor.routing.routing
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.embeddedServer
-import io.ktor.util.KtorExperimentalAPI
-import io.ktor.websocket.WebSockets
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.cio.*
+import io.ktor.util.*
+import io.ktor.websocket.*
 import io.rsocket.kotlin.core.RSocketServerSupport
 import io.rsocket.kotlin.core.rSocket
 
 @OptIn(KtorExperimentalAPI::class)
-fun main() {
-    embeddedServer(CIO, port = 8080) {
-        install(WebSockets)
-        install(RSocketServerSupport)
-        routing {
-            rSocket("/rsocket", acceptor = pingPongAcceptor)
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.rsocket() {
+    install(WebSockets)
+    install(RSocketServerSupport)
+    routing {
+        rSocket("/rsocket", acceptor = pingPongAcceptor)
+    }
+    println("RSocket enabled!")
+}
+
+fun Application.hello() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
         }
-    }.start(true)
+    }
+    println("Http enabled!")
 }

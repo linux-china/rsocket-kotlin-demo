6fedcc17f34add5c90931a105dbd33fa2154b9d2
linux_china <libing.chen@gmail.com>
2020-11-12 00:53:51 -0800

    api adjusted

--- src/main/kotlin/org/mvnsearch/RSocketModule.kt
+++ src/main/kotlin/org/mvnsearch/RSocketModule.kt
@@ -5,35 +5,36 @@ import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.routing.*
 import io.ktor.util.*
+import io.ktor.utils.io.core.*
 import io.ktor.websocket.*
-import io.rsocket.kotlin.RSocketAcceptor
+import io.rsocket.kotlin.ConnectionAcceptor
 import io.rsocket.kotlin.RSocketRequestHandler
-import io.rsocket.kotlin.core.RSocketServerSupport
-import io.rsocket.kotlin.core.rSocket
+import io.rsocket.kotlin.core.RSocketServer
 import io.rsocket.kotlin.payload.Payload
+import io.rsocket.kotlin.transport.ktor.server.RSocketSupport
+import io.rsocket.kotlin.transport.ktor.server.rSocket
+import io.rsocket.kotlin.transport.ktor.serverTransport
 import kotlin.coroutines.CoroutineContext
-
+import kotlin.text.toByteArray
 
 @OptIn(KtorExperimentalAPI::class, InternalAPI::class)
 suspend fun runTcpServer(dispatcher: CoroutineContext, port: Int) {
     println("Start TCP server on 0.0.0.0:${port}")
-    aSocket(SelectorManager(dispatcher))
-        .tcp()
-        .bind("0.0.0.0", port)
-        .rSocket(acceptor = pingPongAcceptor)
+    val transport = aSocket(SelectorManager(dispatcher)).tcp().serverTransport("0.0.0.0", port)
+    RSocketServer().bind(transport, pingPongAcceptor).join()
 }
 
-val pingPongAcceptor: RSocketAcceptor = {
+val pingPongAcceptor: ConnectionAcceptor = ConnectionAcceptor {
     RSocketRequestHandler {
-        requestResponse = {
-            Payload("Pong")
+        requestResponse {
+            Payload(ByteReadPacket("Pong".toByteArray()))
         }
     }
 }
 
 fun Application.rsocket() {
     install(WebSockets)
-    install(RSocketServerSupport)
+    install(RSocketSupport)
     routing {
         rSocket("/rsocket", acceptor = pingPongAcceptor)
     }

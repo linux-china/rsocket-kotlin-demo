c42920773d4680ff14d426bf9c883a361f723383
linux_china <libing.chen@gmail.com>
2022-04-17 13:51:00 -0700

    RSocketServerApp.kt

---
+++ src/main/kotlin/jbang/RSocketServerApp.kt
@@ -0,0 +1,32 @@
+//JAVA 11
+//KOTLIN 1.6.20
+//DEPS io.rsocket.kotlin:rsocket-core-jvm:0.15.4
+//DEPS io.rsocket.kotlin:rsocket-transport-ktor-tcp-jvm:0.15.4
+
+import io.rsocket.kotlin.RSocketRequestHandler
+import io.rsocket.kotlin.core.RSocketServer
+import io.rsocket.kotlin.payload.Payload
+import io.rsocket.kotlin.payload.buildPayload
+import io.rsocket.kotlin.payload.data
+import io.rsocket.kotlin.transport.ktor.tcp.TcpServer
+import io.rsocket.kotlin.transport.ktor.tcp.TcpServerTransport
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    val transport = TcpServerTransport("0.0.0.0", 42252)
+    val server: TcpServer = RSocketServer().bind(transport) {
+        RSocketRequestHandler {
+            //handler for request/response
+            requestResponse { request: Payload ->
+                println(request.data.readText()) //print request payload data
+                buildPayload {
+                    //language=json
+                    data("""{ "data": "Server response" }""")
+                }
+            }
+        }
+    }
+    runBlocking {
+        server.handlerJob.join()
+    }
+}
